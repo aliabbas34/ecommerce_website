5c3e843e7e87d29225564e2ab0b30ed320827a9f
unknown <aliabbas7317@gmail.com>
2023-05-05 12:49:52 +0530

    Cart total amount functionality implemented and conditional rendering of payment and signin button done

--- projfrontend/src/core/StripeCheckout.js
+++ projfrontend/src/core/StripeCheckout.js
@@ -9,9 +9,42 @@ const StripeCheckout = ({
     setRedirect=f=>f, 
     reload=undefined
 })=>{
+
+
+    const [data, setData]=useState({
+        loading:false,
+        success: false,
+        error:"",
+        address: ""
+    });
+
+
+    const token= isAuthenticated() && isAuthenticated().token;
+    const userId= isAuthenticated() && isAuthenticated().user._id;
+
+    const getFinalPrice=()=>{
+        let amount =0;
+        products.map(p=>{
+            amount=amount+p.price;
+        });
+        return amount;
+    };
+
+
+    const showStripeButton=()=>{
+        return isAuthenticated() ? (
+            <button className="btn btn-success">Pay with Stripe</button>
+        ) : (
+            <Link to="/signin">
+                <button className="btn btn-warning">Signin</button>
+            </Link>
+        )
+    }
+
     return(
         <div>
-            <h3 className="text-white">Stripe checkout loaded</h3>
+            <h3 className="text-white">Stripe checkout {getFinalPrice()}</h3>
+            {showStripeButton()}
         </div>
     );
 };
